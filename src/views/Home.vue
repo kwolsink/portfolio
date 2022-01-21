3ed834716362a9a5f140e308baeed9a2d2506973
Kasper <kwolsink@gmail.com>
2022-01-21 23:49:53 +0100

    Add wavy seperators

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="introduction section">
+      <p>
+        Welcome to my online portfolio! My name is Kasper Wolsink and here I try to show off the progress I book during
+        my time as a software developer!
+      </p>
+    </div>
+    <div class="spacer layer1"></div>
+
+    <div class="section">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus amet autem, cum
+      doloribus ea earum eos explicabo fugiat fugit id laboriosam libero nemo nihil quia repellendus sapiente sequi
+      soluta.
+    </div>
+    <div class="section">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus amet autem, cum
+      doloribus ea earum eos explicabo fugiat fugit id laboriosam libero nemo nihil quia repellendus sapiente sequi
+      soluta.
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
+
+<style lang="scss">
+
+.introduction {
+  color: white;
+  background: #ca69fa;
+  font-weight: 900;
+}
+
+.section {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 20vw
+}
+
+.spacer {
+  aspect-ratio: 900/150;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+.layer1 {
+  background-image: url("../assets/layer1.svg");
+}
+
+
+</style>
